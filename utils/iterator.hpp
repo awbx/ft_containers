47ddee6962dab0e5c2c3b476fb7fd220488c8bb1
Abdelhadi Sabani <asabani@student.1337.ma>
2022-11-24 05:15:09 +0100

    chore: add iterator template class

---
+++ utils/iterator.hpp
@@ -0,0 +1,17 @@
+#ifndef __ITERATOR_HPP__
+#define __ITERATOR_HPP__
+
+namespace ft {
+
+template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T *, class Reference = T &>
+struct iterator {
+        typedef T         value_type;
+        typedef Distance  difference_type;
+        typedef Pointer   pointer;
+        typedef Reference reference;
+        typedef Category  iterator_category;
+};
+
+};  // namespace ft
+
+#endif
