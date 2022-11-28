f4099130cb704b9b5fcf516e9373710ea932d707
Abdelhadi Sabani <asabani@student.1337.ma>
2022-11-28 08:53:18 +0100

    chore: use ft iterator tags

--- utils/iterator_traits.hpp
+++ utils/iterator_traits.hpp
@@ -16,21 +16,21 @@ class iterator_traits {
 template <class T>
 class iterator_traits<T *> {
    public:
-    typedef ptrdiff_t                       difference_type;
-    typedef T                               value_type;
-    typedef T                              *pointer;
-    typedef T                              &reference;
-    typedef std::random_access_iterator_tag iterator_category;
+    typedef ptrdiff_t                      difference_type;
+    typedef T                              value_type;
+    typedef T                             *pointer;
+    typedef T                             &reference;
+    typedef ft::random_access_iterator_tag iterator_category;
 };
 
 template <class T>
 class iterator_traits<const T *> {
    public:
-    typedef ptrdiff_t                       difference_type;
-    typedef T                               value_type;
-    typedef const T                        *pointer;
-    typedef const T                        &reference;
-    typedef std::random_access_iterator_tag iterator_category;
+    typedef ptrdiff_t                      difference_type;
+    typedef T                              value_type;
+    typedef const T                       *pointer;
+    typedef const T                       &reference;
+    typedef ft::random_access_iterator_tag iterator_category;
 };
 }  // namespace ft
 
