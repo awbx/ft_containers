391fddf743dbbbc40f30fee25fb0e76df1285819
Abdelhadi Sabani <asabani@student.1337.ma>
2022-11-28 16:36:10 +0100

    chore: make iterator member types public

--- utils/iterator.hpp
+++ utils/iterator.hpp
@@ -7,6 +7,7 @@ namespace ft {
 
 template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T *, class Reference = T &>
 struct iterator {
+   public:
     typedef T         value_type;
     typedef Distance  difference_type;
     typedef Pointer   pointer;
