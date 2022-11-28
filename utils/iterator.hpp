70aca80ee4e2b69d1faec43d346575889abde8f5
Abdelhadi Sabani <asabani@student.1337.ma>
2022-11-28 08:53:00 +0100

    chore: add iterator tags

--- utils/iterator.hpp
+++ utils/iterator.hpp
@@ -3,6 +3,12 @@
 
 namespace ft {
 
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag : public input_iterator_tag, public output_iterator_tag {};
+struct bidirectional_iterator_tag : public forward_iterator_tag {};
+struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
 template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T *, class Reference = T &>
 struct iterator {
     typedef T         value_type;
