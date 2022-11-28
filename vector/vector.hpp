9f6c27d19cc6aed9136ff87156826c9f07fe2885
Abdelhadi Sabani <asabani@student.1337.ma>
2022-11-28 16:37:34 +0100

    chore: add iterators functions

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -3,9 +3,8 @@
 
 #include <memory>
 
-#include "iterator.hpp"
-#include "iterator_traits.hpp"
 #include "reverse_iterator.hpp"
+#include "vector_iterator.hpp"
 
 namespace ft {
 
@@ -13,18 +12,18 @@ template <class T, class Alloc = std::allocator<T> >
 class vector {
    public:
     // member types
-    typedef T                                                           value_type;
-    typedef Alloc                                                       allocator_type;
-    typedef typename allocator_type::reference                          reference;
-    typedef typename allocator_type::const_reference                    const_reference;
-    typedef typename allocator_type::pointer                            pointer;
-    typedef typename allocator_type::const_pointer                      const_pointer;
-    typedef typename allocator_type::size_type                          size_type;
-    typedef ft::iterator<ft::random_access_iterator_tag, pointer>       iterator;
-    typedef ft::iterator<ft::random_access_iterator_tag, const_pointer> const_iterator;
-    typedef ft::reverse_iterator<iterator>                              reverse_iterator;
-    typedef ft::reverse_iterator<const_iterator>                        const_reverse_iterator;
-    typedef typename ft::iterator_traits<iterator>::difference_type     difference_type;
+    typedef T                                                       value_type;
+    typedef Alloc                                                   allocator_type;
+    typedef typename allocator_type::reference                      reference;
+    typedef typename allocator_type::const_reference                const_reference;
+    typedef typename allocator_type::pointer                        pointer;
+    typedef typename allocator_type::const_pointer                  const_pointer;
+    typedef typename allocator_type::size_type                      size_type;
+    typedef ft::vector_iterator<pointer>                            iterator;
+    typedef ft::vector_iterator<const_pointer>                      const_iterator;
+    typedef ft::reverse_iterator<iterator>                          reverse_iterator;
+    typedef ft::reverse_iterator<const_iterator>                    const_reverse_iterator;
+    typedef typename ft::iterator_traits<iterator>::difference_type difference_type;
 
    private:
     pointer        arr;
@@ -78,6 +77,22 @@ class vector {
         // copy `arr` of x to `this->arr`
         std::memcpy(this->arr, x.arr, this->size() * sizeof(value_type));
     }
+
+    // -------------------------------- Iterator function -------------------------------
+
+    iterator       begin() { return iterator(this->arr); };
+    const_iterator begin() const { return const_iterator(this->arr); }
+
+    iterator       end() { return iterator(this->arr + this->size()); }
+    const_iterator end() const { return const_iterator(this->arr + this->size()); }
+
+    reverse_iterator       rbegin() { return reverse_iterator(this->end()); };
+    const_reverse_iterator rbegin() const { return const_reverse_iterator(this->end()); };
+
+    reverse_iterator       rend() { return reverse_iterator(this->begin()); };
+    const_reverse_iterator rend() const { return const_reverse_iterator(this->begin()); };
+
+    // --------------------------------  End of iterator function ------------------------
 
     // -------------------------------- Capacity functions -----------------------------
 
