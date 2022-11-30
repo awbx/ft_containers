e9c55cc7fa3d41795a1c60d8dd231645c5358569
Abdelhadi Sabani <asabani@student.1337.ma>
2022-11-30 15:36:00 +0100

    chore: add vector relational operators

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -345,6 +345,40 @@ class vector {
         this->alloc.deallocate(this->arr, this->capacity());
     }
 };
+
+// relational operators
+template <class T, class Alloc>
+bool operator==(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs) {
+    if (lhs.size() == rhs.size()) {
+        return ft::equal(lhs.begin(), lhs.end(), rhs.begin());
+    }
+    return false;
+}
+template <class T, class Alloc>
+bool operator!=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs) {
+    return !(lhs == rhs);
+}
+
+template <class T, class Alloc>
+bool operator<(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs) {
+    return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+}
+
+template <class T, class Alloc>
+bool operator<=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs) {
+    return !(rhs < lhs);
+}
+
+template <class T, class Alloc>
+bool operator>(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs) {
+    return rhs < lhs;
+}
+
+template <class T, class Alloc>
+bool operator>=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs) {
+    return !(lhs < rhs);
+}
+
 }  // namespace ft
 
 #endif
