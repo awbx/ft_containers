906b67983d34e58523710ee4d8baf1a075cb80a2
Abdelhadi Sabani <asabani@student.1337.ma>
2022-12-02 14:18:51 +0100

    chore: add tests header file

---
+++ tests/tests.hpp
@@ -0,0 +1,21 @@
+#ifndef __TESTS_HPP__
+#define __TESTS_HPP__
+
+#include <deque>     // std::deque
+#include <iomanip>   // std::setw
+#include <iostream>  // std::cout
+#include <stack>     // std::stack
+#include <vector>    // std::vector
+
+#if FT
+namespace ft = std;
+#else
+#include "stack.hpp"  // ft::stack
+
+#endif
+
+void start_test(const char *test_name, void (*func)(void));
+
+void stack_main(void);
+
+#endif
